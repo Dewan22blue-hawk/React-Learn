c64ea0cfb6aa24a51ccbc0409ec1524d1d8e2ca1
Dewan_linux <12221177@bsi.ac.id>
2024-08-13 16:04:15 +0700

    First Setup: Installa tailwind & Vite + ReactJS

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <div className="flex justify-center min-h-screen items-center text-red-200">
+        Hello Vite + React
+      </div>
+    </>
+  );
+}
+
+export default App;
