57ac72d3ac3bdaaca3c4205f0ec2f619843e3293
Dewan_linux <12221177@bsi.ac.id>
2024-08-14 09:56:09 +0700

    Introduction Atomic Design

--- src/App.jsx
+++ src/App.jsx
@@ -1,43 +1,5 @@
 import React from "react";
-
-// kalau pake class component itu bisa statefull component
-// class Button extends React.Component {
-//   render() {
-//     return (
-//       <button
-//         className="h-10 px-6 font-semibold rounded-md bg-black text-white"
-//         type="submit"
-//       >
-//         Buy now class
-//       </button>
-//     );
-//   }
-// }
-// jika pakai function component itu bisa stateless component
-// function ButtonRed() {
-//   return (
-//     <button
-//       className="h-10 px-6 font-semibold rounded-md bg-red-500 text-white"
-//       type="submit"
-//     >
-//       Buy now function
-//     </button>
-//   );
-// }
-// arrow function
-
-const Button = (props) => {
-  const { classes, children } = props;
-  return (
-    <button
-      className={`h-10 px-6 font-semibold rounded-md ${classes} text-white`}
-      type="submit"
-    >
-      {children}
-    </button>
-  );
-};
-
+import Button from "./components/Elements-atom/Button";
 function App() {
   return (
     <div className="">
